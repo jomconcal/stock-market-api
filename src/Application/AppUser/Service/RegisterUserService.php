4123866927125efaf3849883e1c6caed11b21c38
JoseMariaConcha <jomconcal@gmail.com>
2026-05-19 22:06:30 +0200

    return response with token and data

--- src/Application/AppUser/Service/RegisterUserService.php
+++ src/Application/AppUser/Service/RegisterUserService.php
@@ -9,11 +9,13 @@ use App\Application\AppUser\DTO\RegisterUserOutputDto;
 use App\Application\AppUser\Mapper\AppUserFactory;
 use App\Domain\AppUser\Entity\AppUser;
 use App\Domain\AppUser\Repository\AppUserRepositoryInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 class RegisterUserService
 {
     public function __construct(
         private AppUserRepositoryInterface $appUserRepository,
+        private JWTTokenManagerInterface $jwtTokenManager,
     ) {
     }
 
@@ -23,11 +25,13 @@ class RegisterUserService
 
         $existingUser = $this->appUserRepository->findByEmail($appUser->getEmail());
         if ($existingUser instanceof AppUser) {
-            return RegisterUserOutputDto::createFromDuplicated($appUser->getEmail());
+            return RegisterUserOutputDto::createFromDuplicated($appUser);
         }
 
         $this->appUserRepository->save($appUser);
 
-        return RegisterUserOutputDto::createFromRegister($appUser->getEmail());
+        $token = $this->jwtTokenManager->create($appUser);
+
+        return RegisterUserOutputDto::createFromRegister($appUser, $token);
     }
 }
